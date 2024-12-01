1fb24e596861ec30a030dcaa36c75848315ec804
Nikita <clowixdev@gmail.com>
2024-12-01 13:29:53 +0300

    added multithreading to the rows addition process, need to resolve problem with hst_matrix usage

--- GPU/main.cu
+++ GPU/main.cu
@@ -3,28 +3,31 @@
 #include <time.h>
 #include <cuda.h>
 #include <curand.h>
+#include <cuda_runtime.h>
+#include <curand_kernel.h>
 
-#define RANGE 1000
-#define GRID_SIZE 1
 #define BLOCK_SIZE 16
-#define DEPTH 4
 
-/*
-	for (int diag =0; diag<size-1;diag++)
-	{ 
-		for (int str = diag+1; str < size; str++)
-		{
-			float k = (- 1.0) * matrix[str][diag] / matrix[diag][diag];
-			for (int column = diag; column < size; column++)
-			{
-				matrix[str][column] += k * matrix[diag][column] ;
-			}
+__global__ void add_to_row(float* dev_matrix, int row, int diag, float k, int size) {
+    int col = diag + (blockIdx.x * blockDim.x + threadIdx.x);
+
+	if (col < size) {
+		dev_matrix[row*size+col] += k * dev_matrix[diag*size+col];
+	}
+}
+
+void triangular_matrix(float* hst_matrix, float* dev_matrix, int size) {
+	dim3 threadsPerBlock(BLOCK_SIZE, BLOCK_SIZE);
+	dim3 blocksPerGrid((size + threadsPerBlock.x - 1) / threadsPerBlock.x);
+
+	for (int diag = 0; diag < size-1; diag++) {
+		for (int row = diag+1; row < size; row++) {
+			cudaMemcpy(hst_matrix, dev_matrix, sizeof(float) * size * size, cudaMemcpyDeviceToHost);
+
+			float k = (- 1.0) * hst_matrix[row*size+diag] / hst_matrix[diag*size+diag];
+			add_to_row<<<blocksPerGrid, threadsPerBlock>>>(dev_matrix, row, diag, k, size);
 		}
 	}
-*/
-
-__global__ void triangular_matrix(float* matrix, int size) {
-	return;
 }
 
 void show_matrix(float* hst_matrix, int size) {
@@ -34,14 +37,15 @@ void show_matrix(float* hst_matrix, int size) {
 		}
 		printf("\n");
 	}
+	printf("\n");
 }
 
 void fill_matrix(float* dev_matrix, int size) {
     curandGenerator_t gen;
 
     curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
-    curandSetPseudoRandomGeneratorSeed(gen, 0);
-    curandGenerateUniform(gen, dev_matrix, size * size);
+    curandSetPseudoRandomGeneratorSeed(gen, (unsigned long long)clock());
+    curandGenerateUniform(gen, dev_matrix, (size*size));
 
     curandDestroyGenerator(gen);
 }
@@ -53,36 +57,43 @@ int main(void) {
 	fflush(stdin);
 	fscanf(stdin, "%d", &size);
 
-	float *hst_output;
-	float *dev_input;
-	float *dev_output;
+	float *hst_matrix;
+	float *dev_matrix;
 
 	clock_t start = clock(); //! START
 
-    hst_output = (float*)calloc(size * size, sizeof(float));
-    cudaMalloc((void **)&dev_input, sizeof(float) * size * size);
-    cudaMalloc((void **)&dev_output, sizeof(float) * size * size);
+	printf("allocation...");
+    hst_matrix = (float*)calloc(size * size, sizeof(float));
+    cudaMalloc((void **)&dev_matrix, sizeof(float) * size * size);
+	printf(" - allocated.\n");
 
-    fill_matrix(dev_input, size);
+	printf("filling...");
+    fill_matrix(dev_matrix, size);
+	printf(" - filled.\n");
 
-    dim3 dimBlock (BLOCK_SIZE, BLOCK_SIZE);
-    dim3 dimGrid (GRID_SIZE, GRID_SIZE);
-	triangular_matrix <<< dimGrid, dimBlock >>> (dev_input, size);
+	if (size <= 20) {
+		cudaMemcpy(hst_matrix, dev_matrix, sizeof(float) * size * size, cudaMemcpyDeviceToHost);
+		printf("Generated matrix:\n");
+		show_matrix(hst_matrix, size);
+	}
 
-    cudaMemcpy(hst_output, dev_output, size * size, cudaMemcpyDeviceToHost);
-
-	clock_t end = clock(); //! STOP
+	printf("transforming...");
+	triangular_matrix(hst_matrix, dev_matrix, size);
+	printf(" - transformed\n");
 
     if (size <= 20) {
-        show_matrix(hst_output, size);
+		cudaMemcpy(hst_matrix, dev_matrix, sizeof(float) * size * size, cudaMemcpyDeviceToHost);
+		printf("Transformed matrix:\n");
+        show_matrix(hst_matrix, size);
     }
+
+	clock_t end = clock(); //! STOP
 
 	double ms_duration = (double)(end - start) / CLOCKS_PER_SEC * 1000;
 	printf("Time to execute - %f ms\n", ms_duration);
 
-	free(hst_output);
-    cudaFree(dev_input);
-    cudaFree(dev_output);
+	free(hst_matrix);
+    cudaFree(dev_matrix);
 
 	return 0;
 }
