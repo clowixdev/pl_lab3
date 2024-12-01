d7b3bfb6d3a083fb218473ae829b944c786667e3
Nikita <clowixdev@gmail.com>
2024-12-01 13:28:18 +0300

    formatted with <Prettier>

--- CPU/main.c
+++ CPU/main.c
@@ -1,25 +1,23 @@
 
 /*	This program creates a square matrix of size AxA, which is filled with random values.
-*	Then the matrix will be transformed to a triangular matrix using the Gauss method.*/
+ *	Then the matrix will be transformed to a triangular matrix using the Gauss method.*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
 
-#define RANGE 1000		/* [-RANGE;RANGE] for generating random values*/
+#define RANGE 1000 /* [-RANGE;RANGE] for generating random values*/
 #define _CRT_SECURE_NO_WARNINGS
-#define FILE_INPUT
+// #define FILE_INPUT
 
+float **matrix_create(int size);
+void matrix_fill(float **matrix, int size);
+void matrix_clean_up(float **matrix, int size);
+void print_matrix(float **matrix, int size, bool flag);
+void triangular_matrix(float **matrix, int size);
 
-float** matrix_create(int size);
-void matrix_fill(float** matrix, int size);
-void matrix_clean_up(float** matrix, int size);
-void print_matrix(float** matrix, int size, bool flag);
-void triangular_matrix(float** matrix, int size);
-
-
-float** matrix_create(int size)
+float **matrix_create(int size)
 {
 	float **matrix = (float **)malloc(size * sizeof(float *));
 	if (matrix == NULL)
@@ -29,17 +27,17 @@ float** matrix_create(int size)
 	}
 	for (int i = 0; i < size; i++)
 	{
-		matrix[i] = (float*)malloc(size * sizeof(float));
+		matrix[i] = (float *)malloc(size * sizeof(float));
 		if (matrix[i] == NULL)
 		{
 			fprintf(stderr, "Memory allocation failed for matrix columns.\n");
 			exit(EXIT_FAILURE);
 		}
-	} 
+	}
 	return matrix;
 }
 
-void matrix_fill(float** matrix, int size)
+void matrix_fill(float **matrix, int size)
 {
 	srand(time(NULL));
 
@@ -47,23 +45,24 @@ void matrix_fill(float** matrix, int size)
 	{
 		for (int j = 0; j < size; j++)
 		{
-			float value = (float)(rand() % (RANGE*2+1) - RANGE);
+			float value = (float)(rand() % (RANGE * 2 + 1) - RANGE);
 			matrix[i][j] = value;
 		}
 	}
 }
 
-void matrix_clean_up(float** matrix, int size)
+void matrix_clean_up(float **matrix, int size)
 {
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < size; i++)
+	{
 		free(matrix[i]);
 	}
 	free(matrix);
 }
 
-void print_matrix(float** matrix, int size, bool flag)
+void print_matrix(float **matrix, int size, bool flag)
 {
-	FILE* file;
+	FILE *file;
 	if (flag == true)
 	{
 		file = fopen("output.txt", "w");
@@ -74,7 +73,7 @@ void print_matrix(float** matrix, int size, bool flag)
 		file = fopen("output.txt", "a");
 		fprintf(file, "Transformed matrix:\n");
 	}
-	
+
 	if (file == NULL)
 	{
 		printf("Failed to open file\n");
@@ -93,16 +92,16 @@ void print_matrix(float** matrix, int size, bool flag)
 	fclose(file);
 }
 
-void triangular_matrix(float** matrix, int size)
+void triangular_matrix(float **matrix, int size)
 {
-	for (int diag =0; diag<size-1;diag++)
-	{ 
-		for (int str = diag+1; str < size; str++)
+	for (int diag = 0; diag < size - 1; diag++)
+	{
+		for (int str = diag + 1; str < size; str++)
 		{
-			float k = (- 1.0) * matrix[str][diag] / matrix[diag][diag];
+			float k = (-1.0) * matrix[str][diag] / matrix[diag][diag];
 			for (int column = diag; column < size; column++)
 			{
-				matrix[str][column] += k * matrix[diag][column] ;
+				matrix[str][column] += k * matrix[diag][column];
 			}
 		}
 	}
@@ -112,9 +111,9 @@ int main(void)
 {
 
 	int size;
-	float** matrix;
+	float **matrix;
 #ifdef FILE_INPUT
-	FILE* input_file = fopen("input.txt", "r");
+	FILE *input_file = fopen("input.txt", "r");
 	if (input_file == NULL)
 	{
 		printf("Failed to open file\n");
@@ -127,19 +126,18 @@ int main(void)
 	fscanf(stdin, "%d", &size);
 #endif // FILE_INPUT
 
-
 	matrix = matrix_create(size);
 	clock_t start = clock();
 	matrix_fill(matrix, size);
-	
-	//print_matrix(matrix, size, 1);
+
+	// print_matrix(matrix, size, 1);
 
 	triangular_matrix(matrix, size);
 
 	clock_t end = clock();
 	double ms_duration = (double)(end - start) / CLOCKS_PER_SEC * 1000;
 	printf("Time to execute - %f ms\n", ms_duration);
-	//print_matrix(matrix, size, 0);
+	// print_matrix(matrix, size, 0);
 	matrix_clean_up(matrix, size);
 	return 0;
 }
